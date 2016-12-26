1ce57a6b94e7a96e3ae9f37e966f718da8688906
makerprojects <Glitzi85>
2016-12-26 10:15:50 +0100

    Parameter stored in EEProm and managed by simple command interface (1.0)
    This release implements a user interface to set SSID and Password for
    the PiKoder/SSC_wRX w/o reflashing the ESP. A query command would dump
    all parameter settings allowing to verify changes. The default settings
    are: SSID=PiKoder_wRX and password=password.
    
    Use commands "$s=" to set your SSID and "$p=" to change password.
    Command "$?" would dump the latest settings. Please note that <cr><lf>
    is required to start command processing.

--- udpRC_ESP8266-01.ino
+++ udpRC_ESP8266-01.ino
@@ -2,7 +2,12 @@
 //
 // Das ESP8266-01 µC Board wird als Schnittstellenumsetzer zwischen dem wLAN und dem UART
 // des PiKoder/SSC verwendet. Hierzu wird die WLAN-Schnittstelle des Boardes als Access Point 
-// konfiguriert (SSID: "PiKoder_wRX"). In dieses WLAN bucht sich das Smartphone mit der App "udpRC" ein.
+// konfiguriert (default SSID: "PiKoder_wRX", default Password: "password"). 
+// In dieses WLAN bucht sich das Smartphone mit der App "udpRC" ein.
+
+// Last change:
+// 26.12.16: Configuration data stored in EEProm, Commands for setting network parameters
+#define FwVersion "1.0"
 
 // Dieses Programm wird mit der Arduino IDE (Version > 1.6.8) kompiliert und an den µC übertragen
 // werden, wenn man hier unter Datei -> Voreinstellungen -> Additional Boards Manager URLs:
@@ -22,11 +27,40 @@
 //
 // Weitere Informationen auf www.makerprojekte.de und www.pikoder.de.
 
+#include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUDP.h>
 
-const char* ssidap = "PiKoder_wRX"; // SSID
-const char *password = "PASSWORD"; // Password
+// Your local default definitions
+#define YourSSID "PiKoder_wRX"
+#define YourPassPhrase "password"
+
+// ID of the settings block
+#define ConfigHeader "PwRX"
+#define ConfigVersion 1
+
+// Settings structure
+#define CONFIG_START 0
+
+struct StoreStruct {
+  // The variables of the settings
+  char ssidap[32];
+  int SettingsVersion; // consumes 4 Bytes
+  char password[32];
+  // This is for mere detection if they are your settings
+  char CHeader[8]; // it is the last variable of the struct
+  // so when settings are saved, they will only be validated if
+  // they are stored completely.
+} settings = {
+  // The default values
+  YourSSID,
+  ConfigVersion,
+  YourPassPhrase,
+  ConfigHeader
+};
+
+int settingsRetrievedOk = false;
+
 unsigned int localPort = 12001; //Port zum Empfangen der Daten vom Smartphone
 unsigned int remotPort = 12000;
 
@@ -40,16 +74,48 @@ IPAddress targetip(192, 168, 4, 1);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress myIP;
 
-void setup() {
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssidap,password);
-  WiFi.softAPConfig(targetip,targetip,subnet);
+void loadConfig() {
+  // To make sure there are settings, and they are YOURS!
+  // If nothing is found it will use the default settings.
+  if (EEPROM.read(CONFIG_START + sizeof(settings) - sizeof(settings.CHeader) + 4) == settings.CHeader[4] && // this is '\0'
+      EEPROM.read(CONFIG_START + sizeof(settings) - sizeof(settings.CHeader) + 3) == settings.CHeader[3] &&
+      EEPROM.read(CONFIG_START + sizeof(settings) - sizeof(settings.CHeader) + 2) == settings.CHeader[2] &&
+      EEPROM.read(CONFIG_START + sizeof(settings) - sizeof(settings.CHeader) + 1) == settings.CHeader[1] &&
+      EEPROM.read(CONFIG_START + sizeof(settings) - sizeof(settings.CHeader)) == settings.CHeader[0])
+  { // reads settings from EEPROM
+    for (unsigned int t=0; t<sizeof(settings); t++)
+      *((char*)&settings + t) = EEPROM.read(CONFIG_START + t);
+     settingsRetrievedOk = true; 
+  } else {
+    // settings aren't valid! will overwrite with default settings
+    saveConfig();
+  }
+}
 
-  myIP = WiFi.softAPIP();
+void saveConfig() {
+  for (unsigned int t=0; t<sizeof(settings); t++){ // writes to EEPROM
+    EEPROM.write(CONFIG_START + t, *((char*)&settings + t));
+  }
+  EEPROM.commit();  
   
-  Udp.begin(localPort);
+  for (unsigned int t=0; t<sizeof(settings); t++) { // and verifies the data  
+    if (EEPROM.read(CONFIG_START + t) != *((char*)&settings + t)){
+      // error writing to EEPROM
+    }
+  }
+}
 
+
+void setup() {
   Serial.begin(9600); // setup connection to UART
+  delay(1000);
+  EEPROM.begin(sizeof(settings));
+  loadConfig(); 
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(settings.ssidap,settings.password);
+  WiFi.softAPConfig(targetip,targetip,subnet);
+  myIP = WiFi.softAPIP(); 
+  Udp.begin(localPort);
 }
 
 void loop() {
@@ -62,11 +128,73 @@ void loop() {
     } 
   } 
   if (Serial.available() > 0) {
-    Udp.beginPacket(remoteIp, remotPort);
-    while (Serial.available() > 0) {
-      Udp.write(Serial.read());
+    char nChar = Serial.read();
+    if (nChar == '$') {
+      while (!Serial.available()); // wait for next char
+      nChar = Serial.read();
+      if (nChar == '?') { // dump configuration
+        while ((nChar != 0xD && nChar != 0xA)) if (Serial.available() > 0) nChar = Serial.read(); 
+        while ((nChar != 0xD && nChar != 0xA)) if (Serial.available() > 0) nChar = Serial.read(); 
+        Serial.println();
+        Serial.print("Configuration dump ");
+        if (settingsRetrievedOk) {
+          Serial.println("(retrieved from EEPROM)");         
+        } else {
+          Serial.println("(applied default values)");         
+        }
+        Serial.print("Firmware: ");
+        Serial.println(FwVersion);
+        Serial.print("SSID: ");
+        Serial.println(settings.ssidap);
+        Serial.print("Password: ");     
+        Serial.println(settings.password);     
+      } 
+      if (nChar == 'S' || nChar == 's') { // set SSID
+        int i=0;       
+        while ((nChar != 0xD && nChar != 0xA)) {
+          if (Serial.available() > 0) {
+            nChar = Serial.read();
+            if ((!i) && (nChar == '=')) {
+            } else settings.ssidap[i++] = nChar;        
+          }
+        }    
+        settings.ssidap[--i] = '\0';
+        while ((nChar != 0xD && nChar != 0xA)) if (Serial.available() > 0) nChar = Serial.read(); 
+        Serial.println();
+        Serial.print("Setting SSID: ");
+        Serial.println();
+        Serial.print("New SSID: ");
+        Serial.print(settings.ssidap);           
+        saveConfig();
+        Serial.println(" ... reset controller to apply new settings");  
+      }
+      if (nChar == 'P' || nChar == 'p') { // set SSID
+        int i=0;       
+        while ((nChar != 0xD && nChar != 0xA)) {
+          if (Serial.available() > 0) {
+            nChar = Serial.read();
+            if ((!i) && (nChar == '=')) {
+            } else settings.password[i++] = nChar;        
+          }
+        }    
+        settings.password[--i] = '\0';
+        while ((nChar != 0xD && nChar != 0xA)) if (Serial.available() > 0) nChar = Serial.read(); 
+        Serial.println();
+        Serial.print("Setting Password: ");
+        Serial.println();
+        Serial.print("New Password: ");
+        Serial.print(settings.password);           
+        saveConfig();
+        Serial.println(" ... reset controller to apply new settings");  
+      }
+    } else {
+      Udp.beginPacket(remoteIp, remotPort);
+      Udp.write(nChar);
+      while (Serial.available() > 0) {
+        Udp.write(Serial.read());
+      }
+      Udp.endPacket();
     }
-    Udp.endPacket();
   }  
 }    
       
